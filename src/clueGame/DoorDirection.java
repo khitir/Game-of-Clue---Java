9869c085a226b556a85a6a499cd6aab313636f11
John Taylor <johntaylor1309@gmail.com>
2023-10-09 20:35:27 -0600

    John: Added comments and Test.txt to explain test procedure

--- src/clueGame/DoorDirection.java
+++ src/clueGame/DoorDirection.java
@@ -1,5 +1,12 @@
 package clueGame;
 
+/*
+ * Authors: John Taylor and Zakaria Khitirishvili
+ * DoorDirection Enum
+ * 		Acts a s an object that can tell us which direction 
+ * 		to enter or exit a room
+ */
+
 public enum DoorDirection {
 	UP, DOWN, LEFT, RIGHT, NONE
 }
--- src/clueGame/Room.java
+++ src/clueGame/Room.java
@@ -1,11 +1,19 @@
 package clueGame;
 
+/*
+ * Authors: John Taylor and Zakaria Khitirishvili
+ * Room Class
+ * 		Serves as a functional wrapper for each of the rooms in the
+ * 		board game
+ */
+
 public class Room {
 	private String name;
 	private BoardCell centerCell;
 	private BoardCell labelCell;
 
-	
+	// Constructor. Takes in room names, location of room center
+	// and location of room name
 	public Room(String name, BoardCell centerCell, BoardCell labelCell) {
 		super();
 		this.name = name;
@@ -13,26 +21,31 @@ public class Room {
 		this.labelCell = labelCell;
 	}
 
+	// Returns the room's name
 	public String getName() {
 		return name;
 	}
 
+	// Sets the room's name
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	// Returns the center cell of the room
 	public BoardCell getCenterCell() {
 		return centerCell;
 	}
 
+	// Sets the center cell of the room
 	public void setCenterCell(BoardCell centerCell) {
 		this.centerCell = centerCell;
 	}
 
+	// Returns the cell upon which the room's label is drawn
 	public BoardCell getLabelCell() {
 		return labelCell;
 	}
-
+	// Sets the cell where the room's label is drawn
 	public void setLabelCell(BoardCell labelCell) {
 		this.labelCell = labelCell;
 	}
