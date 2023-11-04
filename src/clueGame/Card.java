a8e2d3ea4989e72c91160723b31661b8223d7406
Zakaria Khitirishvili <Khitirishvili@mines.edu>
2023-11-04 12:38:59 -0600

    added CarType enum

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -1,5 +1,31 @@
 package clueGame;
 
 public class Card {
+	private String cardName;
+	
+	
+	public boolean equals(Card target) {
+		if (target.getCardName().equals(cardName)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
+
+	public String getCardName() {
+		return cardName;
+	}
+
+
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	
+	
+	
+	
+	
+	
 }
