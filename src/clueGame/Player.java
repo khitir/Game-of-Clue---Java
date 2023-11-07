616132412cbd68c95dc8d9cd2f787804486d071b
Zakaria Khitirishvili <Khitirishvili@mines.edu>
2023-11-06 17:03:19 -0700

    implemented disprove accusation, but errors

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -1,7 +1,10 @@
 package clueGame;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public abstract class Player {
@@ -11,7 +14,11 @@ public abstract class Player {
 	private boolean isComputer;
 	private boolean isHuman;
 	
-	private Map<String, Card> cards;  // set of cards for each player 
+	private Map<String, Card> cards;  // set of cards for each player, hand 
+	private Set <Card> seenCards;
+	ArrayList<Card> hand = new ArrayList<>(cards.values());
+	
+
 	
 	public Player(String name, String color, boolean isComputer) {
 		this.name = name;
@@ -21,11 +28,56 @@ public abstract class Player {
 		cards = new HashMap<String, Card>(); 
 	}
 	
+	
+	
+	
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+
+
+
 	public void deal(Card card) {
 		cards.put(card.getCardName(), card);
 	}
 	
 	public abstract void updateHand(Card card);
+	
+	public void updateSeen(Card seenCard) {
+		
+	}
+	
+	public Card disproveSuggestion(Card room, Card person, Card weapon) {
+		int matchingCards[];
+		int numMatch = 0;
+		int index_size = 0;
+		ArrayList<Card> matchingCard = new ArrayList<>();
+		
+		for (int i = 0; i < hand.size(); i++) {
+			if (hand.get(i).equals(room) || hand.get(i).equals(person) || hand.get(i).equals(weapon)) {
+				matchingCard.add(hand.get(i));
+			}	
+		}
+		
+		numMatch = matchingCard.size();
+	
+		
+		if (numMatch == 0) {
+			return null;
+		}
+		else if (numMatch == 1) {
+			return matchingCard.get(0);
+		}
+
+		else {
+			Random rand = new Random();
+			int randIndex = rand.nextInt(numMatch);
+			return matchingCard.get(randIndex);
+		}
+	}
+
+		
+	
 	
 	public String getName() {
 		return name;
--- src/tests/GameSolutionTest.java
+++ src/tests/GameSolutionTest.java
@@ -4,18 +4,20 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import clueGame.Board;
 import clueGame.Card;
 import clueGame.CardType;
+import clueGame.Player;
 
 class GameSolutionTest {
 
-	private static Board board;
+	Board board;
 	
-	@BeforeAll
-	public static void setUp() {
+	@BeforeEach
+	public void setUp() {
 		// Board is singleton, get the only instance
 		board = Board.getInstance();
 		// set the file names to use my config files
@@ -26,11 +28,9 @@ class GameSolutionTest {
 	
 	
 	
-	
-	
-	
 	@Test
 	public void testCheckAccusation(){
+
 //		solution that is correct
 		Card roomSolution = board.getSolution().getRoom();
 		Card personSolution = board.getSolution().getPerson();
@@ -52,23 +52,43 @@ class GameSolutionTest {
 		cardRoom.setType(CardType.ROOM);
 		assertFalse(board.checkAccusation(cardRoom, personSolution, weaponSolution));
 		
-	}
+	}	
+//	@Test
+//	public void testDisproveSuggestion(){
+//		board = Board.getInstance();
+//		// set the file names to use my config files
+//		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");		
+//		// Initialize will load config files 
+//		board.initialize();
+//		Player pl1 = board.getPlayers().get(0);
+//		Card card1 = pl1.getHand().get(0);
+//		Card card2 = pl1.getHand().get(1);
+//		Card card3 = pl1.getHand().get(2);
+//		
+//		Card cardToTest = card1;
+//		
+////		If player has only one matching card it should be returned
+//		Card dummy1 = new Card("mess1");
+//		Card dummy2 = new Card("mess2");
+//		
+//		assertTrue(pl1.disproveSuggestion(cardToTest, dummy1, dummy2).equals(card1));
+//		
+////		If players has >1 matching card, returned card should be chosen randomly
+//		assertTrue(pl1.disproveSuggestion(card1, card2, card3).equals(card1) || pl1.disproveSuggestion(card1, card2, card3).equals(card2) || pl1.disproveSuggestion(card1, card2, card3).equals(card3));
+//		assertTrue(pl1.disproveSuggestion(card1, card2, dummy1).equals(card1) || pl1.disproveSuggestion(card1, card2, dummy1).equals(card2));
+//		
+////		If player has no matching cards, null is returned
+//		assertFalse(pl1.disproveSuggestion(dummy1, dummy2, dummy1).equals(card1) || pl1.disproveSuggestion(dummy1, dummy2, dummy1).equals(card2) || pl1.disproveSuggestion(dummy1, dummy2, dummy1).equals(card3));
+//	}
+//	
+//	
 	
-	@Test
-	public void testDisproveSuggestion(){
-//		If player has only one matching card it should be returned
-//		If players has >1 matching card, returned card should be chosen randomly
-//		If player has no matching cards, null is returned
-	}
-	
-	
-	
-	@Test
-	public void testHandleSuggestion(){
-//		Suggestion no one can disprove returns null
-//		Suggestion only suggesting player can disprove returns null
-//		Suggestion only human can disprove returns answer (i.e., card that disproves suggestion)
-//		Suggestion that two players can disprove, correct player (based on starting with next player in list) returns answer
-	}
+//	@Test
+//	public void testHandleSuggestion(){
+////		Suggestion no one can disprove returns null
+////		Suggestion only suggesting player can disprove returns null
+////		Suggestion only human can disprove returns answer (i.e., card that disproves suggestion)
+////		Suggestion that two players can disprove, correct player (based on starting with next player in list) returns answer
+//	}
 
 }
