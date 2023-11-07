a5bd371939b3d8519e6ef86d3b359cd0a501a1c9
John Taylor <johntaylor1309@gmail.com>
2023-11-07 11:25:24 -0700

    Created GUI Class

---
+++ src/clueGame/ClueGameGUI.java
@@ -0,0 +1,12 @@
+package clueGame;
+
+import javax.swing.JFrame;
+
+public class ClueGameGUI extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
